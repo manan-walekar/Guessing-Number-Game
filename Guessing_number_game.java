3066fdd26d67abc3840d707a427258717561271c
Manan <manan.walekar0404@gmail.com>
2024-08-07 21:44:39 +0530

    .\Guessing_number_game.java

---
+++ Guessing_number_game.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Guessing_number_game {
+    public static void main(String[] args) {
+        int randomnumber;
+        int number;
+
+        Scanner scan = new Scanner(System.in);
+
+        Random random = new Random();
+        randomnumber = random.nextInt(100) - 1;
+
+        // System.out.println("RANDOM NO. IS = " + randomnumber);
+        int trycount = 0;
+        while (true) {
+            System.out.println("Guess the number from (1-100) : ");
+            number = scan.nextInt();
+            // System.out.println(number);
+            trycount++;
+
+            if (randomnumber == number) {
+                System.out.println("HURRAY ! YOU WIN");
+                System.out.println("IT TOOK YOU " + trycount + " TRIES :)");
+                break;
+            }
+
+            else if (randomnumber > number) {
+                System.out.println("NOPE THE NUMBER IS GREATER. TRY AGAIN.");
+            } else if (number > 100) {
+                System.out.println("Number is Invalid !");
+                System.out.println("Restart Again");
+                break;
+            }
+
+            else {
+                System.out.println("NOPE THE NUMBER IS SMALLER. TRY AGAIN.");
+            }
+        }
+
+    }
+}
